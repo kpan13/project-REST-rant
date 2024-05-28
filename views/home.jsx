97c2b1242fe9fe13b55f8509f7f8175c962f2b6c
kajal panchal <kajalpanchal@kajals-Air.home>
2024-05-27 22:42:21 -0700

    Adding styles to Rest-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -4,8 +4,15 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
+        <head>
+        <link rel="stylesheet" href="/css/style.css"/>
+        </head>
           <main>
               <h1>HOME</h1>
+              <a href="/places">
+                     <button className="btn-primary">Places Page</button>
+              </a>
+
           </main>
       </Def>
     )
